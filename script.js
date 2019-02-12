d3f1818103dcef87d1d4b5d8933e69afa6b712a0
Philip Sun <philwsun@gmail.com>
2019-02-11 21:58:03 -0500

    Use API call to get what MIME types Google can convert

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const CLIENT_ID = "823146793082-7lgsb7a22pdeilk7vbb8k9ptp7jnfgsm.apps.googleuser
 const SCOPES = "https://www.googleapis.com/auth/drive.file";
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 
-var signedin = false;
+var stage = 0;
 var signin_status = document.getElementById("signin-status");
 var signin_button = document.getElementById("signin-button");
 var file_input = document.getElementById("file-input");
+
+var mimeReplacements = {};
 
 function handleClientLoad() {
   gapi.load("client:auth2", initClient);
@@ -28,14 +30,13 @@ function initClient() {
 }
 
 function updateSigninStatus(isSignedIn) {
-  signedin = isSignedIn;
-  if (signedin) {
-    signin_status.innerHTML = "Signed in";
-    signin_button.style.display = "none";
-  } else {
-    signin_status.innerHTML = "Not Signed In";
-    signin_button.style.display = "inline-block";
-  }
+  if (!isSignedIn) return;
+
+  stage = 1;
+  getMimeReplacements();
+
+  signin_status.innerHTML = "Signed in";
+  signin_button.style.display = "none";
 }
 
 function signin(event) {
@@ -43,11 +44,36 @@ function signin(event) {
 }
 
 function upload() {
-  if (!signedin) {
-    console.log("not signed in, can't do it");
+  if (stage !== 2) {
+    console.log("not ready");
     return;
   }
   upload_file(file_input.files[0]);
+}
+
+function getMimeReplacements() {
+  if (stage !== 1) {
+    console.log("not signed in, can't do it");
+    return;
+  }
+  gapi.client.drive.about.get({
+    fields: "importFormats"
+  }).then(
+    response => {
+      resp = response.result.importFormats;
+      mimeReplacements = {};
+      for (var key in resp) {
+        if (Array.isArray(resp[key]) && resp[key].length === 1)
+          mimeReplacements[key] = resp[key][0];
+        else
+          console.log("Strange import format: ", key, resp[key]);
+      }
+      if (stage === 1) stage = 2;
+    },
+    err => {
+      console.error("err: ", err)
+    }
+  );
 }
 
 function upload_file(file) {
@@ -56,7 +82,10 @@ function upload_file(file) {
   const BOUNDARY = "--ASD_ASD_ASD_123456789_987654321_YUH_YUH";
   readFile(file).then(
     base64File => {
-      console.log(base64File);
+      var mimeType = file.type;
+      if (mimeType in mimeReplacements)
+        mimeType = mimeReplacements[mimeType];
+
       gapi.client.request({
         path: "upload/drive/v3/files",
         method: "POST",
@@ -64,10 +93,10 @@ function upload_file(file) {
         headers: {
           "Content-type": `multipart/related; boundary=${BOUNDARY}`,
         },
-        body: formatMultipartBody("ayo2.png", file.type, base64File, BOUNDARY)
+        body: formatMultipartBody("ayo2.png", mimeType, base64File, BOUNDARY)
       }).then(
         response => console.log("upload success ", response),
-        err => console.error("upload error ", error)
+        err => console.error("upload error ", err)
       );
     }
   );
